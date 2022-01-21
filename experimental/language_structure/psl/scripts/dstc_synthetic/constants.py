d8c6132212c71ffa42c06a6f8969dc60bc9caf03
Connor Pryor <cfpryor@ucsc.edu>
2022-01-21 12:59:15 -0800

    Added dstc_synthetic analysis.

---
+++ experimental/language_structure/psl/scripts/dstc_synthetic/constants.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+# Copyright 2021 The Uncertainty Baselines Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+
+"""DSTC Synthetic constants."""
+
+RULE_WEIGHTS = [1.0, 20.0, 5.0, 5.0, 5.0, 10.0, 5.0, 20.0, 5.0, 5.0, 5.0, 10.0]
+RULE_NAMES = ('rule_1', 'rule_2', 'rule_3', 'rule_4', 'rule_5', 'rule_6',
+              'rule_7', 'rule_8', 'rule_9', 'rule_10', 'rule_11', 'rule_12')
+
+DATA_CONFIG = {
+    'batch_size': 128,
+    'max_dialog_size': 10,
+    'max_utterance_size': 40,
+    'class_map': {
+        'accept': 0,
+        'cancel': 1,
+        'end': 2,
+        'greet': 3,
+        'info_question': 4,
+        'init_request': 5,
+        'insist': 6,
+        'second_request': 7,
+        'slot_question': 8,
+    },
+    'accept_words': ['yes', 'great'],
+    'cancel_words': ['no'],
+    'end_words': ['thank', 'thanks'],
+    'greet_words': ['hello', 'hi'],
+    'info_question_words': ['address', 'phone'],
+    'insist_words': ['sure', 'no'],
+    'slot_question_words': ['what', '?'],
+    'includes_word': -1,
+    'excludes_word': -2,
+    'mask_index': 0,
+    'accept_index': 1,
+    'cancel_index': 2,
+    'end_index': 3,
+    'greet_index': 4,
+    'info_question_index': 5,
+    'insist_index': 6,
+    'slot_question_index': 7,
+    'utterance_mask': -1,
+    'last_utterance_mask': -2,
+    'pad_utterance_mask': -3,
+}
+
+KWARGS_DICT = {
+    'inference.constrained_beam_search_decoding.ConstrainedBeamSearchDecoding':
+        {'num_beams': 3, 'class_rules_parities': [-1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1],
+         'class_rules_mapping': {0: [7], 1: [8], 2: [6], 3: [0, 1], 4: [9], 5: [2, 4, 5], 6: [10], 7: [3], 8: [11]}},
+    'inference.constrained_gradient_decoding.ConstrainedGradientDecoding':
+        {'alpha': 0.1, 'grad_steps': 25},
+    'inference.unconstrained_inference.UnconstrainedInference':
+        {},
+    'learning.constrained_regularized_learning.ConstrainedRegularizedLearning':
+        {'epochs': 100},
+    'learning.unconstrained_learning.UnconstrainedLearning':
+        {'epochs': 1},
+    'models.multiwoz_synthetic.psl_model.PSLModelMultiWoZ':
+        {'rule_weights': RULE_WEIGHTS, 'rule_names': RULE_NAMES, 'config': DATA_CONFIG},
+    'scripts.multiwoz_synthetic.model_util':
+        {'input_size': [DATA_CONFIG['max_dialog_size'], DATA_CONFIG['max_utterance_size']]},
+}
---
+++ experimental/language_structure/psl/scripts/dstc_synthetic/data_util.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+# Copyright 2021 The Uncertainty Baselines Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""DSCT Synthetic data manipulation."""
+
+import logging
+import os
+import sys
+
+import scripts.logs as logs
+import scripts.util as util
+import uncertainty_baselines.datasets.dialog_state_tracking as data_loader
+
+import tensorflow as tf
+
+def prepare_dataset(data_dir, config):
+    """Prepares the train and test datasets."""
+    train_data_loader = data_loader.SGDSynthDataset(data_dir, split="train")
+    # train_ds = train_data_loader.load(batch_size=32768)
+    train_ds = train_data_loader.load(batch_size=1024)
+
+    test_data_loader = data_loader.SGDSynthDataset(data_dir, split="test")
+    # test_ds = test_data_loader.load(batch_size=32768)
+    test_ds = test_data_loader.load(batch_size=1024)
+
+    # analyze_data(train_ds)
+
+def analyze_data(dataset):
+    usr_label_dict = {}
+    sys_label_dict = {}
+    state_transitions_dict = {0: {}, -1: {}}
+
+    last_label = -1
+    for batch in dataset:
+        for usr_dialogue, sys_dialogue, labels in zip(batch['usr_utt_raw'], batch['sys_utt_raw'], batch['label']):
+            if labels[0].numpy() not in state_transitions_dict[0]:
+                state_transitions_dict[0][labels[0].numpy()] = 0
+            state_transitions_dict[0][labels[0].numpy()] += 1
+
+            for usr_utt, sys_utt, label in zip(usr_dialogue, sys_dialogue, labels):
+                if label == 0:
+                    if last_label not in state_transitions_dict[-1]:
+                        state_transitions_dict[-1][last_label] = 0
+                    state_transitions_dict[-1][last_label] += 1
+                    break
+                if label.numpy() not in state_transitions_dict:
+                    state_transitions_dict[label.numpy()] = {}
+
+                if last_label not in state_transitions_dict[label.numpy()]:
+                    state_transitions_dict[label.numpy()][last_label] = 0
+                state_transitions_dict[label.numpy()][last_label] += 1
+                last_label = label.numpy()
+
+                if label.numpy() not in usr_label_dict:
+                    usr_label_dict[label.numpy()] = {}
+                for usr_word in usr_utt.numpy().split():
+                    if usr_word not in usr_label_dict[label.numpy()]:
+                        usr_label_dict[label.numpy()][usr_word] = 0
+                    usr_label_dict[label.numpy()][usr_word] += 1
+
+                if label.numpy() not in sys_label_dict:
+                    sys_label_dict[label.numpy()] = {}
+                for sys_word in sys_utt.numpy().split():
+                    if sys_word not in sys_label_dict[label.numpy()]:
+                        sys_label_dict[label.numpy()][sys_word] = 0
+                    sys_label_dict[label.numpy()][sys_word] += 1
+        break
+
+    for label_name, transition_dict in sorted(state_transitions_dict.items()):
+        print("\n\nUser State: %d\n" % (label_name,))
+        sorted_transition_dict = sorted(transition_dict, key=transition_dict.get, reverse=True)[:5]
+        for transition in sorted_transition_dict:
+            print("Previous State: %s Count: %d" % (transition, transition_dict[transition]))
+
+    for label_name, word_dict in sorted(usr_label_dict.items()):
+        print("\n\nSystem State: %d\n" % (label_name,))
+        sorted_word_dict = sorted(word_dict, key=word_dict.get, reverse=True)[:50]
+        for word in sorted_word_dict:
+            print("Word: %s Count: %d" % (word, word_dict[word]))
+
+    for label_name, word_dict in sorted(sys_label_dict.items()):
+        print("\n\nState: %d\n" % (label_name,))
+        sorted_word_dict = sorted(word_dict, key=word_dict.get, reverse=True)[:50]
+        for word in sorted_word_dict:
+            print("Word: %s Count: %d" % (word, word_dict[word]))
+
+def main(data_dir):
+    prepare_dataset(data_dir, {})
+
+def _load_args(args):
+    executable = args.pop(0);
+    if len(args) != 1 or ({'h', 'help'} & {arg.lower().strip().replace('-', '') for arg in args}):
+        print('USAGE: python3 %s <data directory>' % (executable,), file=sys.stderr)
+        sys.exit(1)
+
+    data_dir = args.pop(0)
+
+    return (data_dir,)
+
+if __name__ == '__main__':
+    logs.initLogging()
+    main(*_load_args(sys.argv))
